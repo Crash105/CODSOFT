e9362c8639a70fa6772055c9054858b2b148acf7
Crash107 <brianm201720@gmail.com>
2024-12-04 22:23:02 -0500

    add files

--- numberguesser.java
+++ numberguesser.java
@@ -13,39 +13,40 @@ public class numberguesser {
 
         int attempts = 4;
         int rounds = 3;
+        int userscore = 100;
 
         System.out.println(
                 "Guess a random number between 1 to 100. You will have 3 rounds of 4 attempts each to guess the number. ");
 
-        System.out.println(
-                "User Score is calculated by every round being worth 4 points and evey attempt worth 2 points. Your current User Score is 20");
-
         while (scanner.hasNext()) {
 
+            System.out.println("You Are on Round:" + rounds + " Attempt:" + (attempts - 1));
             int guess = scanner.nextInt();
 
             if (guess > number) {
                 System.out.println("Your Guess Is To High");
                 attempts--;
+
             } else if (guess < number) {
                 System.out.println("Your guess is To Low");
+
                 attempts--;
             } else if (guess == number) {
                 System.out.println("Your Guess is Correct");
+
                 break;
 
             }
-            if (attempts == 0) {
+            if (rounds == 1 && attempts == 0) {
+                System.out.println("You are out of rounds");
+
+                break;
+            } else if (attempts == 0) {
                 attempts = 4;
                 rounds--;
                 System.out.println("You are on Round: " + rounds);
-            } else if (rounds == 0) {
-                System.out.println("You are out of rounds");
-                break;
             }
 
-            int userscore = (rounds * 4) + (attempts * 2);
-            System.out.println("Current UserScore: " + userscore);
         }
         scanner.close();
 
